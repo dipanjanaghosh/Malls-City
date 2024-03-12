f6b300ec52fe28d4f1e90802279f14b67240f9b6
unknown <dipanjana.ghosh@bandhan.org>
2024-03-12 20:34:22 +0530

    modified header and added search city component

--- malls-city-ui/src/app/shared/shared-routing.module.ts
+++ malls-city-ui/src/app/shared/shared-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchCityComponent } from './components/search-city/search-city.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:SearchCityComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- malls-city-ui/src/app/shared/shared.module.ts
+++ malls-city-ui/src/app/shared/shared.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {FormsModule} from '@angular/forms';
 import { SharedRoutingModule } from './shared-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MaterialModule } from '../Material.Module';
-
+import { SearchCityComponent } from './components/search-city/search-city.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SearchCityComponent
   ],
   imports: [
     CommonModule,
     SharedRoutingModule,
-    MaterialModule
+    MaterialModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule
   ],
   exports: [
     HeaderComponent
