de15055c188cc0fa5fc8e6b453f34bfbfad22188
piyushroy007 <piyush9051roy@gmail.com>
2024-08-26 18:15:05 +0530

    Header test cases written

--- malls-city-ui/src/app/shared/components/header/header.component.spec.ts
+++ malls-city-ui/src/app/shared/components/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MaterialModule } from 'src/app/Material.Module';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,14 +10,28 @@ describe('HeaderComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      declarations: [HeaderComponent],
+      imports: [MaterialModule],
     });
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should have headerTitle', () => {
+    const headerTitle: string = 'Malls@City';
+    let h1 = fixture.nativeElement.querySelector('.headerTitle');
+    console.log('Should have headerTitle---------', h1.textContent);
+    expect(h1.textContent).toEqual(headerTitle);
+  });
+
+  it('Should have 3 buttons', () => {
+    let h1 = fixture.debugElement.nativeElement.querySelectorAll('button');
+    console.log('Should have 3 buttons -----:', h1.length);
+    expect(h1.length).toEqual(3);
   });
 });
