f456fa424fcdd00a8c125dac39f02a07ec49dc7d
piyushroy007 <piyush9051roy@gmail.com>
2024-09-19 17:36:01 +0530

    test cases for Home

--- malls-city-ui/src/app/shared/components/home/home.component.spec.ts
+++ malls-city-ui/src/app/shared/components/home/home.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { SearchCityService } from '../../services/search-city.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormField } from '@angular/material/form-field';
 
-describe('HomeComponent', () => {
+fdescribe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [HomeComponent],
+      providers: [SearchCityService],
+      imports: [
+        HttpClientModule,
+        ReactiveFormsModule,
+        FormsModule,
+        MatFormField,
+      ],
     });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
@@ -16,6 +27,12 @@ describe('HomeComponent', () => {
   });
 
   it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have a dropdown with test select your city', () => {
+    let dropdwon = fixture.debugElement.nativeElement.querySelector('.bg');
+    console.log('dropdwon', dropdwon.textContent);
     expect(component).toBeTruthy();
   });
 });
